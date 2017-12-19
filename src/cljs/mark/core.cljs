54b410b8e0324f8918413e5ab831e1737caa09a1
dimovich <dimovich@gmail.com>
2017-12-19 03:19:09 +0200

    ..

--- src/cljs/mark/core.cljs
+++ src/cljs/mark/core.cljs
@@ -1,14 +1,43 @@
 (ns mark.core
   (:require ;;[taoensso.timbre :refer [info]]
-            [dommy.core :as d]
-            [hipo.core :as h]))
+            [dommy.core :as d]))
 
 
 (defn toggle-play [base el]
-  (let [img (d/sel1 (d/sel1 base :.flex-active-slide) :img)]
-    (d/toggle-class! img :spin)
+  (let [img (d/sel1 base :img)]
+    (if (d/has-class? img :spin)
+      (d/toggle-class! img :spin-pause)
+      (d/add-class! img :spin))
+    
     (d/toggle-class! el :play)
     (d/toggle-class! el :pause)))
+
+
+(defn pass-on-bg [base]
+  (let [els (d/sel base :.vinyl-wrapper)
+        cls (map second (re-seq #"(\bbg[-_][\w]*)" (d/class base)))]
+    (doseq [cl cls]
+      (d/remove-class! base cl)
+      (doseq [el els]
+        (d/add-class! el cl)))))
+
+
+(defn wrap-divs [base]
+  (let [els (d/sel base :li)
+        spacers (d/sel base :.vinyl-wrapper)]
+    (when (empty? spacers)
+      (doseq [el els]
+        (let [html (.-innerHTML el)]
+          (set! (.-innerHTML el)
+                (str "<div class=\"vinyl-wrapper\">"
+                     html
+                     "<div class=\"vinyl-control play\"></div>"
+                     "</div>")))))))
+
+
+(defn add-listen [base]
+  (let [el (d/sel1 base :.vinyl-control)]
+    (d/listen! el :click #(toggle-play base el))))
 
 
 (defn ^:export init []
@@ -17,11 +46,11 @@
               (if (empty? views)
                 (js/setTimeout checker 1000)
                 (do
-              ;;    (info "views:" views)
                   (doseq [v views]
-                    (let [el (h/create [:div.vinyl-control.play])]
-                      (d/listen! el :click #(toggle-play v el))
-                      (.appendChild v el)))))))]
+                    (wrap-divs v)
+                    (doseq [div (d/sel v :.vinyl-wrapper)]
+                      (add-listen div)))
+                  (doall (map pass-on-bg (d/sel :.wpb_gallery)))))))]
     (checker)))
 
 
